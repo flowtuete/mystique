1a6e18b9e542790f1bb2732930d46ed0dbafd442
michischeid <michischeid@web.de>
2017-04-05 21:50:35 +0200

    implemented evolution module
    implemented first evolution module and complete fitness function

--- 1-Library/urlLib.js
+++ 1-Library/urlLib.js
@@ -1,6 +1,11 @@
 // See a working example: https://jsfiddle.net/39kvmxd4/11/
 // Open DEV Tools to see the result
 // Disable WebSecurity for CORS requests
+/* Libs:
+	https://code.jquery.com/jquery-3.2.1.min.js
+	https://cdn.jsdelivr.net/bluebird/3.5.0/bluebird.js
+*/
+
 
 var urlLib = {
 
@@ -20,22 +25,123 @@ var urlLib = {
           resolve(this.generateURL(config));
         }).catch((err) => {
           console.log("Regenerate");
-          return this.generateURL(config);
+          //return this.generateURL(config);
         });
     })
 
   },
 
 
+  //------------- evolutionary algorithm------------------------------------------------------------------
+  //------------------------------------------------------------------------------------------------------
+
+  //-----fitness function--------------------------------------------------------------------------------
+  _evolutionModule: {
+    wordSorter: function(a, b) {
+      return a.score - b.score
+    },
+    updateLexicon : function(config) {      
+      return new Promise(function(resolve, reject) {
+        //TODO: find new words (score can be zero in the first iteration)
+        
+        var wordsWithScores = [{
+          name: "Geld",
+          score: 2
+        }, {
+          name: "Finanzen",
+          score: 0
+        }];
+        var personaKey = "Bank";
+        wordsWithScores.sort(urlLib._evolutionModule.wordSorter);
+       
+        return urlLib._evolutionModule.insertWordIfFitEnough("Holz", wordsWithScores, personaKey);
+      });
+    },
+    insertWordIfFitEnough : function(word, words, masterWord) {
+      return new Promise(function(resolve, reject) {
+        var getNumOfResults = function(search) {
+          return new Promise(function(resolve, reject) {
+            var url = "https://www.google.com/search?q=" + encodeURIComponent(search);            
+            $.get({
+              url: url,
+              success: function(data) {
+                var resultStatsRegexp = /<div id="resultStats">[^>]*</g;
+                var resultStats = resultStatsRegexp.exec(data)[0].replace("<div id=\"resultStats\">", "").replace("<", "").replace(" Ergebnisse", "").replace(" Results", "").replace("Ungefähr ", "");
+                var numberOfResults = parseInt(resultStats.replace(/\./g, ''));
+                return resolve(numberOfResults);
+              },
+              error: function(e) {
+                reject(e);
+              }
+            });
+          });
+        };
+
+        //check that every word has a result
+
+				
+        function buildScoresIfNotThere(words) {
+          if (words[0].score > 0) {
+            // assume all scores are there
+            return new Promise(function(resolve, reject) {
+              resolve(words);
+            });
+          }
+          //get all scores
+          return Promise.each(words, item => {
+            return getNumOfResults(item.name + " " + masterWord)
+              .then((score) => {
+                item.score = score;
+              })
+          }).then((words) => {
+            console.log("Built all word scores!");
+            words.sort(urlLib._evolutionModule.wordSorter);
+            return Promise.resolve(words);
+          })
+        };
+
+        buildScoresIfNotThere(words).then(function() {
+          console.log(JSON.stringify(words));
+
+          //get worst word in words
+          var worstWord = words.shift();
+          getNumOfResults(word + " " + masterWord).then(function(scoreNew) {
+            //give the oldWord a last chance
+            getNumOfResults(worstWord.name + " " + masterWord).then(function(scoreOld) {
+              //console.log("Fitnessfunction("+word+"): "+worstWord .name+" "+masterWord+": "+scoreOld+ " vs. "+word +" "+masterWord+": "+scoreNew);
+              if (scoreNew > scoreOld) {
+                words.push({
+                  name: word,
+                  score: scoreNew
+                });
+                console.log("Fitnessfunction: " + worstWord.name + " replaced by " + word + "(" + scoreNew + ")");
+              } else {
+                //update oldWord
+                worstWord.score = scoreOld;
+                words.push(worstWord);
+                console.log("Fitnessfunction: " + word + "(" + scoreNew + ")" + " was not able to replace " + worstWord.name);
+              }
+              words.sort(urlLib._evolutionModule.wordSorter);
+            });
+          });
+        });
+
+      });
+    }
+  },
+
+
   _buildSearchString(config) {
     return new Promise(function(resolve, reject) {
-      // Find new Words!
-      var words = [];
-      words.push(_get_random_word(config.wordlist));
-      words.push(_get_random_word(config.wordlist));
-      var searchString = words.join(" and ");
-
-      resolve(searchString);
+      console.log("build search string" + urlLib._evolutionModule);
+      urlLib._evolutionModule.updateLexicon(config).then(function(config) {
+        console.log("updated lexicon");
+        var words = [];
+        words.push(_get_random_word(config.wordlist));
+        words.push(_get_random_word(config.wordlist));
+        var searchString = words.join(" and ");
+        resolve(searchString);
+      });
     });
 
     function _get_random_word(w) {
@@ -78,7 +184,7 @@ var urlLib = {
 
 
 
-}
+};
 
 var wl = ["abacus", "abbey", "abdomen", "ability", "abolishment", "abroad", "accelerant", "accelerator", "accident", "accompanist", "accordion", "account", "accountant", "achieve", "achiever", "acid", "acknowledgment", "acoustic", "acoustics", "acrylic", "act", "action", "active", "activity", "actor", "actress", "acupuncture", "ad", "adapter", "addiction", "addition", "address", "adjustment", "administration", "adrenalin"];
 
