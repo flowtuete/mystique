18859d537bb8c25a1150069c9f7d3836b4033bf6
bensteUEM <GiPJa,15>
2017-04-18 14:56:59 +0200

    interim step for #11 still needs testing

--- 3-Firefox/mystique/mystique.js
+++ 3-Firefox/mystique/mystique.js
@@ -1,20 +1,72 @@
- //alert("Mystique Addon loads on Firefox and the DHBW pages");
+//SAMPLE Code in order to run the functionality
  
- var links = getLinks();
- selectLink(links,true);
+var links = getLinks(true);
+selectLink(links);
  
- // Gets all Links from the currently open page
- function getLinks() {  
+
+/**
+* getLinks is used to get a list of all links in the specified document link
+*/
+//TODO untested
+function getLinksPure(){
 	var array = [];
-	var links = document.getElementsByTagName("a");
-	for(var i=0; i<links.length; i++) {
-		array.push(links[i].href);
+	var linksDetected = document.getElementsByTagName("a");
+	for(var i=0; i<linksDetected.length; i++) {
+		array.push(linksDetected[i].href);
+	}
+
+//TODO shorten with true
+	return array;
+}
+
+
+/**
+* getLinks is used to get a list of all links in the specified 
+@param document link
+@param followLinkOnDomainOnly to filter only to same Domain links
+*/
+//TODO untested
+function getLinks(followLinkOnDomainOnly){
+	var linksDetected = getLinksPure();
+	var array = [];
+	for(var i=0; i<linksDetected.length; i++) {
+		if (isOnSameDomain(document.location.href,linksDetected[i])){
+			array.push(linksDetected[i]);
+		}
+		else if (!followLinkOnDomainOnly) {
+			array.push(linksDetected[i]);
+		}
+
 	}
 	return array;
 }
 
+
+/**
+* select links is used to get a number of links based on the
+*/
+
+function isOnSameDomain(currentPage,checkPage){
+	//TODO implement #11
+
+	var prefix = /^https?:\/\//i;
+    	var domain = /^[^\/]+/;
+    	// removing prefix
+    	url1 = currentPage.replace(prefix, "");
+	url2 = checkPage.replace(prefix, "");
+    	// if link starts with / it is on the current page
+	if (url2.charAt(0) === "/") {
+        	return true;
+    	}
+	
+    	// now extract just the domain
+   	var part1 = url1.match(domain)
+   	var part2 = url1.match(domain)
+	return part1 = part2;
+}
+
 //Should select one link to be opened
-function selectLink(links,siteOnly){
+function selectLink(links){
 	window.alert("This page has got "+links.length+" links");
 	//TODO this is not implemented yet
 }
